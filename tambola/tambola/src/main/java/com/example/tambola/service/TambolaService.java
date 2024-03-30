505dc9cce5fc615eff477f646756aa24d572dd26
Kush-24 <kukkuyd@gmail.com>
2024-03-30 20:19:10 +0530

    rm tambola

--- tambola/tambola/src/main/java/com/example/tambola/service/TambolaService.java
+++
@@ -1,9 +0,0 @@
-package com.example.tambola.service;
-
-import java.util.List;
-
-import com.example.tambola.entiry.TambolaUserCard;
-
-public interface TambolaService {
-    public TambolaUserCard generateCustomTambolaCard();
-}
